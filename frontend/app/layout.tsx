b51ff081eb9fbe631d610c6fe5cd54421d47252e
Soham Ghugare <soham.ghugare@gmail.com>
2025-07-06 01:24:23 +0200

    refactor: removed unused imports

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,11 +3,7 @@ import { Lato } from "next/font/google";
 import "./globals.css";
 import Navbar from './components/Navbar';
 import { ClerkProvider } from '@clerk/nextjs';
-import '@rainbow-me/rainbowkit/styles.css';
 import { Providers } from './providers';
-import { FlowProvider } from "@onflow/kit";
-
-import flowJson from "../flow.json"
 
 const lato = Lato({
   weight: ['100', '300', '400', '700', '900'],
